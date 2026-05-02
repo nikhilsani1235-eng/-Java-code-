3d53afdf27546bd005f533c2cb5112f8f76d8692
Nikhil Saini <nikhilsani1235@gmail.com>
2026-05-02 22:38:20 +0530

    star

--- GFG.java
+++ GFG.java
@@ -1,18 +1,28 @@
 
-
-class GFG {
+public class GFG {
     public static void main(String[] args)
     {
-        int a = 15, b = 25;
+        int num = 5;
+        int x = 0;
+        for (int i = 1; i <= num; i++) {
+            x = i - 1;
+            for (int j = i; j <= num - 1; j++) {
 
-        int ans = (a > b) ? a : b;
-        while (true) {
-            if (ans % a == 0 && ans % b == 0)
-                break;
-            ans++;
+                System.out.print(" ");
+
+                System.out.print("  ");
+            }
+
+            for (int j = 0; j <= x; j++)
+                System.out.print((i + j) < 10
+                                     ? (i + j) + "  "
+                                     : (i + j) + " ");
+
+            for (int j = 1; j <= x; j++)
+                System.out.print((i + x - j) < 10
+                                     ? (i + x - j) + "  "
+                                     : (i + x - j) + " ");
+            System.out.println();
         }
-
-        System.out.println("LCM of " + a + " and " + b
-                           + " : " + ans);
     }
 }
