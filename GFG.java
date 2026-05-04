12141ab35d9ce6c789a06ea3946f272d8be3927f
Nikhil Saini <nikhilsani1235@gmail.com>
2026-05-04 11:45:50 +0530

    pyramid

--- GFG.java
+++ GFG.java
@@ -1,11 +1,11 @@
 public class GFG {
-    public static void main(String[] args) {
-        int rows = 9;
-        for (int i = rows - 1; i >= 0; i--) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print("* ");
-            }
+    public static void PyramidStar(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) System.out.print("* ");
             System.out.println();
         }
+    }
+    public static void main(String[] args) {
+        PyramidStar(5);
     }
 }
