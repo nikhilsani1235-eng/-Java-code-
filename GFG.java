a4978aebf8fac1d4ab8f1ed9df4cf8e288b37fe4
Nikhil Saini <nikhilsani1235@gmail.com>
2026-05-04 11:44:34 +0530

    stardownward

--- GFG.java
+++ GFG.java
@@ -1,27 +1,10 @@
-
 public class GFG {
-    public static void main(String[] args)
-    {
-        int num = 5;
-        int x = 0;
-        for (int i = 1; i <= num; i++) {
-            x = i - 1;
-            for (int j = i; j <= num - 1; j++) {
-
-                System.out.print(" ");
-
-                System.out.print("  ");
+    public static void main(String[] args) {
+        int rows = 9;
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
             }
-
-            for (int j = 0; j <= x; j++)
-                System.out.print((i + j) < 10
-                                     ? (i + j) + "  "
-                                     : (i + j) + " ");
-
-            for (int j = 1; j <= x; j++)
-                System.out.print((i + x - j) < 10
-                                     ? (i + x - j) + "  "
-                                     : (i + x - j) + " ");
             System.out.println();
         }
     }
