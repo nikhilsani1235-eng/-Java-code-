bd66bb67b7f0433b122e59118a45ac6c898e2420
Nikhil Saini <nikhilsani1235@gmail.com>
2026-05-04 11:27:17 +0530

    printer

--- Main.java
+++ Main.java
@@ -1,59 +1,37 @@
-class Transport {
-    void move() {
-        System.out.println("Transport is moving");
-    }
-
-    void fuel() {
-        System.out.println("Transport uses fuel");
-    }
+interface Printer {
+    void printing();
 }
 
-class Train extends Transport {
-    void move() {
-        System.out.println("Train moves on tracks");
-    }
-
-    void fuel() {
-        System.out.println("Train uses diesel or electricity");
-    }
+interface Scanner {
+    void scanning();
 }
 
-class Bus extends Transport {
-    void move() {
-        System.out.println("Bus moves on roads using fuel");
-    }
-
-    void fuel() {
-        System.out.println("Bus uses diesel or petrol");
-    }
+interface Fax {
+    void faxing();
 }
- 
-class Aeroplane extends Transport {
-    void move() {
-        System.out.println("Aeroplane flies in the sky");
+
+class AllInOne implements Printer, Scanner, Fax {
+    @Override
+    public void printing() {
+        System.out.println("Printing document...");
     }
 
-    void fuel() {
-        System.out.println("Aeroplane uses aviation fuel");
+    @Override
+    public void scanning() {
+        System.out.println("Scanning document...");
+    }
+
+    @Override
+    public void faxing() {
+        System.out.println("Sending fax...");
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Train t = new Train();
-        Bus b = new Bus();
-        Aeroplane a = new Aeroplane();
-
-        System.out.println("Train:");
-        t.move();
-        t.fuel();
-
-        System.out.println("\nBus:");
-        b.move();
-        b.fuel();
-
-        System.out.println("\nAeroplane:");
-        a.move();
-        a.fuel();
+        AllInOne device = new AllInOne();
+        device.printing();
+        device.scanning();
+        device.faxing();
     }
 }
