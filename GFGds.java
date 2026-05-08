9edcfe3f91dc28586601472d814a1b33e55135f7
Nikhil Saini <nikhilsani1235@gmail.com>
2026-05-08 22:17:05 +0530

    Converting into Long

---
+++ GFGds.java
@@ -0,0 +1,17 @@
+
+
+public class GFG {
+
+    public static void main(String args[]) {
+        
+        // Creating a custom string
+        String s = "999999999999";
+        System.out.println("String: " + s);
+
+        // Converting into Long
+        long l = Long.parseLong(s);
+
+        // Printing String as Long
+        System.out.println("Long: " + l);
+    }
+}
