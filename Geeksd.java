0642f32ea22a8e12690cd9be1b03e1eccdbf7f36
Nikhil Saini <nikhilsani1235@gmail.com>
2026-05-08 22:22:05 +0530

    Geeks

---
+++ Geeksd.java
@@ -0,0 +1,19 @@
+
+
+// Driver Class
+class Geeks
+{
+
+    // Main method
+    public static void main(String[] args)
+    {
+        // Custom integer input
+        int i = 97;
+
+        // Type casting character to integer
+        char ch = (char)i;
+
+        // Print statement
+        System.out.println(ch);
+    }
+}
