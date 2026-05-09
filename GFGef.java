c50b3016efed36138b82409183d51acec150639e
Nikhil Saini <nikhilsani1235@gmail.com>
2026-05-09 22:06:18 +0530

    GFGef

---
+++ GFGef.java
@@ -0,0 +1,20 @@
+
+import java.io.*;
+
+// Main class
+class  GFGef{
+
+    // Main driver method
+    public static void main(String[] args)
+    {
+        // Creating a StringBuilder object
+        // with a String pass as parameter
+        StringBuilder str
+            = new StringBuilder("GeeksForGeeks");
+
+        // Print and display the string
+        // using standard toString() method
+        System.out.println("String contains = "
+                           + str.toString());
+    }
+}
