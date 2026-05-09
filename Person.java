67e56f9dbcfbc9cc091e7993034b3c1365609cc0
Nikhil Saini <nikhilsani1235@gmail.com>
2026-05-09 21:59:20 +0530

    Geeks

--- Person.java
+++ Person.java
@@ -1,5 +1,10 @@
 class Person {
     void role() { System.out.println("I am a person."); }
+
+    public void setName(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setName'");
+    }
 }
 class Father extends Person {
     @Override void role()
