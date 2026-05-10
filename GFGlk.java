beb17102e30e31a0450263904dda65bd7446a3b8
Nikhil Saini <nikhilsani1235@gmail.com>
2026-05-10 18:07:01 +0530

    Sorting the array

---
+++ GFGlk.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+class GFGlk {
+    public static void main(String[] args)
+    {
+        Integer a[] = { 1, 2, 3, 4, 5 };
+
+    
+        Arrays.sort(a, Collections.reverseOrder());
+
+        System.out.println(Arrays.toString(a));
+    }
+}
