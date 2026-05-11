24098a165df4d06dd9922732e5d65a1ece787851
Nikhil Saini <nikhilsani1235@gmail.com>
2026-05-11 21:05:01 +0530

    elements in ascending order

---
+++ GFGlp.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class GFGlp {
+  	public static void main(String[] args) {
+      
+        int[] arr = new int[] { -2, 0, 1, 3, -1, 2 };
+
+        // use Arrays.sort() method to sort array
+        // elements in ascending order
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
