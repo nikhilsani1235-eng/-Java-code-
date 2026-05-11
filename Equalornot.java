76522fcb9702d67c8a71f616ea16ff38404eba11
Nikhil Saini <nikhilsani1235@gmail.com>
2026-05-11 21:15:42 +0530

    Equalornot

---
+++ Equalornot.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Equalornot {
+    public static void main(String[] args) {
+
+        int a[] = {10, 20, 30};
+        int b[] = {10, 20, 30};
+
+        // Using Arrays.equals() to check 
+        // if two arrays are equal
+        boolean r = Arrays.equals(a, b);
+
+        if (r) {
+            System.out.println("Equal");
+        } else {
+            System.out.println("Not Equal");
+        }
+    }
+}
