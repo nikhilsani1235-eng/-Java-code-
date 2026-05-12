c8710bb83dcdbd43d175892d80d6310de4331a93
Nikhil Saini <nikhilsani1235@gmail.com>
2026-05-12 23:42:31 +0530

    Prepend

---
+++ Prepend.java
@@ -0,0 +1,15 @@
+class Prepend {
+    
+    public static void main(String[] args){
+        
+        String s = "Geeks";
+        String r = "";
+
+        for (int i = 0; i < s.length(); i++){
+            
+            // prepend each character
+            r = s.charAt(i) + r; 
+        }
+        System.out.println(r);
+    }
+}
