9bf64168662830a069edf259f9505b16f82ac4a2
Nikhil Saini <nikhilsani1235@gmail.com>
2026-05-12 23:24:39 +0530

    Driver

---
+++ Driver].java
@@ -0,0 +1,19 @@
+
+class Driver{
+
+  
+    public static void main(String[] args)
+    {
+
+        String str = "GeeksForGeeks";
+
+        // Get the index
+        int index = 5;
+
+        char ch = str.charAt(index);
+
+        System.out.println("Character from " + str
+                           + " at index " + index
+                           + " is " + ch);
+    }
+}
