000056c33a5f47c3fe7a26c2b871c238be2589ae
Nikhil Saini <nikhilsani1235@gmail.com>
2026-05-13 22:38:02 +0530

    CompareStrings

---
+++ CompareStrings.java
@@ -0,0 +1,15 @@
+// Java Program to compare two strings 
+// using equals() method
+public class CompareStrings {
+  
+    public static void main(String[] args) {
+      
+        String s1 = "Hello";
+        String s2 = "Geeks";
+        String s3 = "Hello";
+
+        // Comparing strings
+        System.out.println(s1.equals(s2)); 
+        System.out.println(s1.equals(s3));
+    }
+}
