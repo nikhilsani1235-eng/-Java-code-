fdc11b4181c53955e7ff4a26eb45f242c36da11c
Nikhil Saini <nikhilsani1235@gmail.com>
2026-05-16 18:48:19 +0530

    LinkedHashSets

---
+++ LinkedHashSets.java
@@ -0,0 +1,44 @@
+
+import java.util.*;
+
+public class LinkedHashSets {
+    public static void main(String args[])
+    {
+
+        // create a ArrayList String type
+        ArrayList<String>
+            gfg = new ArrayList<String>();
+
+        // Initialize an ArrayList
+        gfg.add("Geeks");
+        gfg.add("for");
+        gfg.add("Geeks");
+
+        // print ArrayList
+        System.out.println("Original ArrayList : "
+                           + gfg);
+
+        // -----Using LinkedHashSet-----
+        System.out.println("\nUsing LinkedHashSet:\n");
+
+        // create a set and copy all value of list
+        Set<String> set = new LinkedHashSet<>(gfg);
+
+        // create a list and copy all value of set
+        List<String> gfg1 = new ArrayList<>(set);
+
+        // print ArrayList
+        System.out.println("Modified ArrayList : "
+                           + gfg1);
+
+        // -----Using HashSet-----
+        System.out.println("\nUsing HashSet:\n");
+
+        // create a list and copy all value of set
+        List<String> gfg2 = new ArrayList<>(set);
+
+        // print ArrayList
+        System.out.println("Modified ArrayList : "
+                           + gfg2);
+    }
+}
