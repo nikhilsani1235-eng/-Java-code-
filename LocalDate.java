c793e5dee5a8dad26039643e7e93d995dfdbaa64
Nikhil Saini <nikhilsani1235@gmail.com>
2026-05-16 18:51:12 +0530

    LocalDate

---
+++ LocalDate.java
@@ -0,0 +1,22 @@
+// Java Program to Display Dates in Different Format
+
+
+
+class LocalDate {
+
+    // Main driver method
+    public static void main(String[] args)
+    {
+        // Create an object of LoacalDate type
+        LocalDate date = LocalDate.now();
+        // .now() method to tore the current date
+
+        // Print current date
+        System.out.println(date);
+    }
+
+    private static LocalDate now() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'now'");
+    }
+}
