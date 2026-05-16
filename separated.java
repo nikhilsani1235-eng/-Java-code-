9773cdf35130ee5ae69d865618e1e32fd394f86e
Nikhil Saini <nikhilsani1235@gmail.com>
2026-05-16 18:44:38 +0530

    ArrayLists

--- separated.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class separated {
-    public static void main(String args[]) {
-        List<String> list = new ArrayList<>(Arrays.asList("Geeks", "ForGeeks", "GeeksForGeeks"));
-        System.out.println("List of String: " + list);
-        String result = String.join(",", list);
-        System.out.println("Comma separated String: " + result);
-    }
-}
