53f606072d668a795cc1e2f8f75f9efe77bd22d0
Nikhil Saini <nikhilsani1235@gmail.com>
2026-05-17 22:11:41 +0530

    Directory

---
+++ Directory.java
@@ -0,0 +1,14 @@
+
+
+public class Directory {
+  
+    public static void main(String[] args)
+    {
+
+        
+        String path = System.getProperty("user.dir");
+
+        
+        System.out.println("Working Directory = " + path);
+    }
+}
--- GFG.java
+++ GFG.java
@@ -1,3 +1,8 @@
 public class GFG {
 
+    public void file(String string, String string2, int i) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'file'");
+    }
+
 }
