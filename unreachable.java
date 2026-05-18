3507d97e6118c15c0a3d447c0e1b712076b1ecb3
Nikhil Saini <nikhilsani1235@gmail.com>
2026-05-18 21:30:27 +0530

    unreachable

---
+++ unreachable.java
@@ -0,0 +1,12 @@
+class unreachable {
+    /**
+     * @param args
+     */
+    public static void main(String args[])
+    {
+
+        System.out.println("I will get printed");
+
+        return;
+    }
+}
