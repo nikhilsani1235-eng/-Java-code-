935a2a9b4d013a548322ab2794036e331060c826
Nikhil Saini <nikhilsani1235@gmail.com>
2026-05-18 21:28:48 +0530

    ArithmeticException

---
+++ ArithmeticException.java
@@ -0,0 +1,10 @@
+
+class ArithmeticException {
+    public static void main(String[] args)
+    {
+        int a = 6;
+        int b = 0;
+        System.out.print(a / b);
+        // this line Throw  ArithmeticException: / by zero
+    }
+}
